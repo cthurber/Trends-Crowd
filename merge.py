68718aa97ee75412a5e7d217885409a1ed14b05f
Chris Thurber <cthurber@me.com>
2017-03-21 15:47:41 -0400

    Merge now maps all files in the cache to a dataframe dict; next step: use pd.concat to join unique values

--- merge.py
+++ merge.py
@@ -18,23 +18,23 @@ output_path = "./data/"
 def getRootFeed(filename): return re.findall("(.*)__", filename)[0]
 def getFilenameKeys(cache_path): return list(set(map(getRootFeed, os.listdir(cache_path))))
 def getNumCols(filename): return len(open(filename).readline().split(','))
-def toDataFrame(filename): return pd.read_csv(filename,index_col=0)
+def toDataFrame(filename,icol=0): return pd.read_csv(filename,index_col=int(icol))
+def masterFileExists(feedname,feeddir): True if(feedname in os.listdir(feeddir)) else False
 
 # Returns {feedname : [DataFrame1,DataFrame2, ...]} pairing
 def mapDataFrames(cache_path):
     filenames = {}
     for key in getFilenameKeys(cache_path):
-        filenames[key] = []
-        for filename in os.listdir(cache_path):
-            if key == getRootFeed(filename):
-                filenames[key].append(toDataFrame(cache_path + str(filename)))
+        filenames[key] = [toDataFrame(cache_path + str(filename)) for filename in os.listdir(cache_path) if key == getRootFeed(filename)]
     return filenames
 
 df_index = (mapDataFrames(cache_path))
+print(df_index)
 
-# Check if masterfile exists?
+# for feed in index
+if(masterFileExists(feed,output_path)):
+    # merge all new dataframes with master from 'data' folder
 #   if no: write merged version from df_index
-#   if yes: merge all new dataframes with master from 'data' folder
 
 # Write merged version
 """
