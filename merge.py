dd57812c26bcd570891ecb422d07ba3d522abe73
Chris Thurber <cthurber@me.com>
2017-03-22 23:59:45 -0400

    Merge now dedupes based on Dates column, merges

--- merge.py
+++ merge.py
@@ -18,25 +18,22 @@ def toDataFrame(filename,icol=0): return pd.read_csv(filename,index_col=int(icol
 
 # Returns {feedname : [DataFrame1,DataFrame2, ...]} pairing
 def mapDataFrames(cache_path):
-    # Further efficiency?: dict.fromkeys([1, 2, 3, 4])
     filenames = {}
     for key in getFilenames(cache_path):
         filenames[key] = [toDataFrame(cache_path + str(filename)) for filename in os.listdir(cache_path) if key == getFeedName(filename)]
     return filenames
 
-# REMOVE FOR FINAL COPY, REQUIRE CMD ARGS
-cache_path = "./cache/csv/"
-output_path = "./data/"
+def mergeFeeds(cache_path="./cache/csv/", output_path="./data/"):
 
-index = mapDataFrames(cache_path)
+    index = mapDataFrames(cache_path)
 
-for feed in index:
-    if not os.path.exists(output_path): os.makedirs(output_path)
-    if(os.path.exists(output_path + feed)):
-        index[feed].append(toDataFrame(output_path + feed + ".csv"))
-    # df = mergeFrames(index[feed]) # This should leave only unique values
-    df = pd.concat(index[feed])
-    df = df.drop_duplicates(subset='Date')
-    # Make caching so as to not lose data on bad write
-    df.to_csv(output_path + feed + ".csv")
-    # Clear cache
+    for feed in index:
+        if not os.path.exists(output_path): os.makedirs(output_path)
+        if(os.path.exists(output_path + feed)):
+            index[feed].append(toDataFrame(output_path + feed + ".csv"))
+
+        df = pd.concat(index[feed])
+        df = df.drop_duplicates(subset='Date')
+        # Make caching so as to not lose data on bad write
+        df.to_csv(output_path + feed + ".csv")
+        # Clear cache
